1fa1bb5a57e231c721834b06a80f57f90b73fb9c
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2023-06-07 06:15:09 +0500

    Add files via upload

--- demo.py
+++ demo.py
@@ -2,36 +2,34 @@ from os import get_terminal_size as gts
 from time import sleep as sl
 from pixpile import *
 
-clr = "\033[0;0m\033[H\033[2J\033[3J"
-
 def render(fps: int):
     canvas = list(gts())
     canvas[1] += -1
     """Renders an "image" to the screen."""
     try:
         while True:
-            print(draw_rectangle("@", 117, 0, 0, canvas[0], canvas[1]))
-            print(draw_rectangle("g", 40, 0, canvas[1]-3, canvas[0], 7))
-            print(draw_rectangle("h", 208, canvas[0]-23, canvas[1]-13, 21, 10))
-            print(draw_rectangle("d", 9, canvas[0]-21, canvas[1]-9, 5, 6))
-            print(draw_rectangle("w", 11, canvas[0]-14, canvas[1]-10, 10, 5))
-            print(draw_rectangle("f", 208, canvas[0]-10, canvas[1]-10, 2, 5))
-            print(draw_rectangle("f", 208, canvas[0]-14, canvas[1]-8, 10, 1))
-            print(draw_rectangle("h", 11, canvas[0]-18, canvas[1]-7, 1, 2))
-            draw_ellipse("s", 11, 16, 7, 10, 5)
-            draw_ellipse("s", 11, 16, 7, 9, 5)
-            draw_ellipse("s", 11, 16, 7, 8, 5)
-            print(draw_rectangle("s", 11, 9, 3, 15, 9))
-            draw_ellipse("l", 21, 20, canvas[1]-4, 15, 2)
-            print(draw_rectangle("l", 21, 6, canvas[1]-5, 29, 3))
-            draw_line("r", 9, canvas[0]-24, canvas[1]-12, canvas[0]-13, canvas[1]-18)
-            draw_line("r", 9, canvas[0]-13, canvas[1]-18, canvas[0]-2, canvas[1]-12)
-            draw_line("r", 9, canvas[0]-24, canvas[1]-12, canvas[0]-2, canvas[1]-12)
-            draw_line("r", 9, canvas[0]-21, canvas[1]-13, canvas[0]-5, canvas[1]-13)
-            draw_line("r", 9, canvas[0]-19, canvas[1]-14, canvas[0]-7, canvas[1]-14)
-            draw_line("r", 9, canvas[0]-17, canvas[1]-15, canvas[0]-9, canvas[1]-15)
-            draw_line("r", 9, canvas[0]-15, canvas[1]-16, canvas[0]-11, canvas[1]-16)
-            print(draw_pixel("r", 9, canvas[0]-13, canvas[1]-17))
+            print(drawRectangle("@", 117, 0, 0, 0, canvas[0], canvas[1]))
+            print(drawRectangle("g", 40, 0, 0, canvas[1]-3, canvas[0], 7))
+            print(drawRectangle("h", 208, 0, canvas[0]-23, canvas[1]-13, 21, 10))
+            print(drawRectangle("d", 9, 0, canvas[0]-21, canvas[1]-9, 5, 6))
+            print(drawRectangle("w", 11, 0, canvas[0]-14, canvas[1]-10, 10, 5))
+            print(drawRectangle("f", 208, 0, canvas[0]-10, canvas[1]-10, 2, 5))
+            print(drawRectangle("f", 208, 0, canvas[0]-14, canvas[1]-8, 10, 1))
+            print(drawRectangle("h", 11, 0, canvas[0]-18, canvas[1]-7, 1, 2))
+            drawEllipse("s", 11, 0, 16, 7, 10, 5)
+            drawEllipse("s", 11, 0, 16, 7, 9, 5)
+            drawEllipse("s", 11, 0, 16, 7, 8, 5)
+            print(drawRectangle("s", 11, 0, 9, 3, 15, 9))
+            drawEllipse("l", 21, 0, 20, canvas[1]-4, 15, 2)
+            print(drawRectangle("l", 21, 0, 6, canvas[1]-5, 29, 3))
+            drawLine("r", 9, 0, canvas[0]-24, canvas[1]-12, canvas[0]-13, canvas[1]-18)
+            drawLine("r", 9, 0, canvas[0]-13, canvas[1]-18, canvas[0]-2, canvas[1]-12)
+            drawLine("r", 9, 0, canvas[0]-24, canvas[1]-12, canvas[0]-2, canvas[1]-12)
+            drawLine("r", 9, 0, canvas[0]-21, canvas[1]-13, canvas[0]-5, canvas[1]-13)
+            drawLine("r", 9, 0, canvas[0]-19, canvas[1]-14, canvas[0]-7, canvas[1]-14)
+            drawLine("r", 9, 0, canvas[0]-17, canvas[1]-15, canvas[0]-9, canvas[1]-15)
+            drawLine("r", 9, 0, canvas[0]-15, canvas[1]-16, canvas[0]-11, canvas[1]-16)
+            print(drawPixel("r", 9, 0, canvas[0]-13, canvas[1]-17))
             sl(1/fps)
             canvas = list(gts())
             canvas[1] += -1
@@ -40,9 +38,9 @@ def render(fps: int):
 
 def main():
     """A function responsible for initialization"""
-    print(clr, end="")
+    print(CLR, end="")
     render(10)
-    print(clr, end="")
+    print(CLR, end="")
 
 if __name__ == "__main__":
     main()
--- pixpile.py
+++ pixpile.py
@@ -1,9 +1,20 @@
-def check_collision(rect1: list, rect2: int, colltype: int):
+ESC = "\033"
+CLR = f"{ESC}[0;0m{ESC}[H{ESC}[2J{ESC}[3J"
+
+def colorIt(text, colorIdText, colorIdBackground):
+    """Colors text"""
+    return f'{ESC}[38;5;{colorIdText}m{ESC}[48;5;{colorIdBackground}m{text}{ESC}[0;0m'
+
+def moveCursor(posX, posY):
+    """Moves cursor to (posX, posY)"""
+    return f"{ESC}[{posY};{posX}H"
+
+def checkCollision(rect1: list, rect2: int, colltype: int):
     """Checks collisions with the edge of other obj.
 
-    rect1 = [posx[0], posy[1], sizex[2], sizey[3], speedx[4], speedy[5]]
+    rect1 = [posX[0], posY[1], sizeX[2], sizeY[3], speedX[4], speedY[5]]
     
-    rect2 = [posx[0], posy[1], sizex[2], sizey[3], speedx[4], speedy[5]]
+    rect2 = [posX[0], posY[1], sizeX[2], sizeY[3], speedX[4], speedY[5]]
     """
     
     coll = False
@@ -37,61 +48,57 @@ def check_collision(rect1: list, rect2: int, colltype: int):
     
     return rect1[0], rect1[1], rect1[2], rect1[3], rect1[4], rect1[5], coll
 
-
-def draw_pixel(sym: str, color_id: int, posx: int, posy: int):
+def drawPixel(sym: str, colorIdFg: int, colorIdBg: int, posX: int, posY: int):
     """Draws a pixel on a canvas"""
-    return f"\033[38;5;{color_id}m\033[{posy+1};{posx+1}H{sym}"
+    return f"{ESC}[38;5;{colorIdFg}m{ESC}[48;5;{colorIdBg}m{ESC}[{posY+1};{posX+1}H{sym}"
 
-
-def draw_rectangle(sym: str, color_id: int, posx: int, posy: int, geomx: int, geomy: int):
+def drawRectangle(sym: str, colorIdFg: int, colorIdBg: int, posX: int, posY: int, geomX: int, geomY: int):
     """Draws a rectangle on a canvas"""
-    line = sym*geomx
-    rect = f"\033[38;5;{color_id}m"
-    for g_y in range(geomy):
-        rect += f"\033[{posy+1+g_y};{posx+1}H{line}"
+    line = sym*geomX
+    rect = f"{ESC}[38;5;{colorIdFg}m{ESC}[48;5;{colorIdBg}m"
+    for g_y in range(geomY):
+        rect += f"{ESC}[{posY+1+g_y};{posX+1}H{line}"
     return rect
 
-
-def draw_ellipse(sym: str, color_id: int, posx: int, posy: int, radx: int, rady: int):
+def drawEllipse(sym: str, colorIdFg: int, colorIdBg: int, posX: int, posY: int, radX: int, radY: int):
     """Draws a ellipse on a canvas"""
     x = 0
-    y = rady
-    d1 = rady**2 - radx**2 * rady + 0.25 * radx**2
-    dx = x * 2 * rady**2
-    dy = y * 2 * radx**2
+    y = radY
+    d1 = radY**2 - radX**2 * radY + 0.25 * radX**2
+    dx = x * 2 * radY**2
+    dy = y * 2 * radX**2
     while dx < dy:
-        print(draw_pixel(sym, color_id, x+posx, y+posy))
-        print(draw_pixel(sym, color_id, -x+posx, y+posy))
-        print(draw_pixel(sym, color_id, x+posx, -y+posy))
-        print(draw_pixel(sym, color_id, -x+posx, -y+posy))
+        print(drawPixel(sym, colorIdFg, colorIdBg, x+posX, y+posY))
+        print(drawPixel(sym, colorIdFg, colorIdBg, -x+posX, y+posY))
+        print(drawPixel(sym, colorIdFg, colorIdBg, x+posX, -y+posY))
+        print(drawPixel(sym, colorIdFg, colorIdBg, -x+posX, -y+posY))
         if d1 < 0:
             x += 1
-            dx += 2 * rady**2
-            d1 += dx + rady**2
+            dx += 2 * radY**2
+            d1 += dx + radY**2
         else:
             x += 1
             y -= 1
-            dx += 2 * rady**2
-            dy -= 2 * radx**2
-            d1 += dx - dy + rady**2
-    d2 = (rady**2 * (x + 0.5)**2) + \
-        (radx**2 * (y - 1)**2) - (radx**2 * rady**2)
+            dx += 2 * radY**2
+            dy -= 2 * radX**2
+            d1 += dx - dy + radY**2
+    d2 = (radY**2 * (x + 0.5)**2) + \
+        (radX**2 * (y - 1)**2) - (radX**2 * radY**2)
     for y in range(y, -1, -1):
-        print(draw_pixel(sym, color_id, x+posx, y+posy))
-        print(draw_pixel(sym, color_id, -x+posx, y+posy))
-        print(draw_pixel(sym, color_id, x+posx, -y+posy))
-        print(draw_pixel(sym, color_id, -x+posx, -y+posy))
+        print(drawPixel(sym, colorIdFg, colorIdBg, x+posX, y+posY))
+        print(drawPixel(sym, colorIdFg, colorIdBg, -x+posX, y+posY))
+        print(drawPixel(sym, colorIdFg, colorIdBg, x+posX, -y+posY))
+        print(drawPixel(sym, colorIdFg, colorIdBg, -x+posX, -y+posY))
         if d2 > 0:
-            dy -= 2 * radx**2
-            d2 += radx**2 - dy
+            dy -= 2 * radX**2
+            d2 += radX**2 - dy
         else:
             x += 1
-            dx += 2 * rady**2
-            dy -= 2 * radx**2
-            d2 += dx - dy + radx**2
+            dx += 2 * radY**2
+            dy -= 2 * radX**2
+            d2 += dx - dy + radX**2
 
-
-def draw_line(sym: str, color_id: int, x0: int, y0: int, x1: int, y1: int):
+def drawLine(sym: str, colorIdFg: int, colorIdBg: int, x0: int, y0: int, x1: int, y1: int):
     """Draws a line on a canvas"""
     dx = x1 - x0
     dy = y1 - y0
@@ -103,8 +110,8 @@ def draw_line(sym: str, color_id: int, x0: int, y0: int, x1: int, y1: int):
     ddy = dy / length
     x = x0
     y = y0
-    print(draw_pixel(sym, color_id, round(x), round(y)))
+    print(drawPixel(sym, colorIdFg, colorIdBg, round(x), round(y)))
     for i in range(length):
         x += ddx
         y += ddy
-        print(draw_pixel(sym, color_id, round(x), round(y)))
+        print(drawPixel(sym, colorIdFg, colorIdBg, round(x), round(y)))
