5bad9e1af9fce32c7991eef9fb645944c555401e
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2023-06-10 02:01:36 +0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
 from os import get_terminal_size as gts
 from time import sleep as sl
-from pypixpile.pypixpile import *
+from pypixpile import *
 
 def render(fps: int):
     canvas = list(gts())
-    canvas = (canvas[0]-1, canvas[1])
+    canvas = (canvas[0]-1, canvas[1]-1)
     """Renders an "image" to the screen."""
     try:
         while True:
@@ -16,13 +16,14 @@ def render(fps: int):
             drawRectangle("f", 208, 0, canvas[0]-10, canvas[1]-10, 2, 5)
             drawRectangle("f", 208, 0, canvas[0]-14, canvas[1]-8 , 10, 1)
             drawRectangle("h", 11, 0, canvas[0]-18, canvas[1]-7 , 1, 1) 
-            drawLine("r", 9, 0, canvas[0]-24, canvas[1]-13, canvas[0]-11, canvas[1]-19)
-            drawLine("r", 9, 0, canvas[0]-13, canvas[1]-19, canvas[0], canvas[1]-12)
-            drawLine("r", 9, 0, canvas[0]-24, canvas[1]-13, canvas[0], canvas[1]-13)
-            drawLine("r", 9, 0, canvas[0]-21, canvas[1]-14, canvas[0]-2, canvas[1]-14)
-            drawLine("r", 9, 0, canvas[0]-19, canvas[1]-15, canvas[0]-4, canvas[1]-15)
-            drawLine("r", 9, 0, canvas[0]-17, canvas[1]-16, canvas[0]-6, canvas[1]-16)
-            drawLine("r", 9, 0, canvas[0]-15, canvas[1]-17, canvas[0]-8, canvas[1]-17)
+            drawLine("r", 9, 0, canvas[0]-24, canvas[1]-13, canvas[0]-13, canvas[1]-19)
+            drawLine("r", 9, 0, canvas[0]-13, canvas[1]-19, canvas[0]-2, canvas[1]-13)
+            drawLine("r", 9, 0, canvas[0]-24, canvas[1]-13, canvas[0]-2, canvas[1]-13)
+            drawLine("r", 9, 0, canvas[0]-23, canvas[1]-14, canvas[0]-3, canvas[1]-14)
+            drawLine("r", 9, 0, canvas[0]-21, canvas[1]-15, canvas[0]-5, canvas[1]-15)
+            drawLine("r", 9, 0, canvas[0]-19, canvas[1]-16, canvas[0]-7, canvas[1]-16)
+            drawLine("r", 9, 0, canvas[0]-17, canvas[1]-17, canvas[0]-9, canvas[1]-17)
+            drawPixel("r", 9, 0, canvas[0]-13, canvas[1]-18)
             drawEllipse("s", 11, 0, 16, 7, 10, 5)
             drawEllipse("l", 21, 0, 20, canvas[1]-4, 15, 2)
             sl(1/fps)
